62c5b26ecb9ba0d936aaffc3c7daa5fd59d3423d
AmriArshad <amri.arshad16@yahoo.com>
2020-10-21 13:02:13 +1300

    updating from Bounce-II

---
+++ src/bounce/DynamicRectangleShape.java
@@ -0,0 +1,46 @@
+package bounce;
+
+import java.awt.Color;
+
+/**
+ * Class to represent a dynamic rectangle.
+ * 
+ */
+public class DynamicRectangleShape extends RectangleShape{
+
+	protected Color COLOR = Color.red;
+	protected boolean bounced = false;
+
+    public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
+        super(x, y, deltaX, deltaY, width, height);
+    }
+
+    public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color){
+        super(x, y, deltaX, deltaY, width, height);
+        COLOR = color;
+    }
+
+    public void doPaint(Painter painter) {
+		if (bounced == false){
+			painter.setColor(COLOR);
+			painter.fillRect(_x,_y,_width,_height);
+			painter.setColor(Color.black);	
+		}
+		else{
+			super.doPaint(painter);
+		}
+    }
+
+    @Override
+    public void move(int width, int height) {
+		super.move(width, height);
+
+		if (x() == 0 || x() == width - width()){
+			bounced = false;
+		}
+		else if (y() == 0 || y() == height - height()){
+			bounced = true;
+		}
+	}
+
+}
--- src/bounce/ImageRectangleShape.java
+++ src/bounce/ImageRectangleShape.java
@@ -1,26 +1,59 @@
 package bounce;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
 
 /**
- * Class to represent a rectangle that displays an image.
  * 
- * @author Ian Warren
- * 
+ * Class to represent an image.
  */
 public class ImageRectangleShape extends RectangleShape {
+    private Image image;
+    protected static int sw;
+    protected static int sh;
 
-	private Image _picture;
-	
-	public ImageRectangleShape(int deltaX, int deltaY, Image image) {
-		// Derive the shape's width and height from the image.
-		super(2, 2, deltaX, deltaY, image.getWidth(null), image.getHeight(null));
-		
-		_picture = image;
-	}
-	
-	@Override
-	protected void doPaint(Painter painter) {
-		painter.drawImage(_picture,_x,_y,_width,_height);
-	}
+    public ImageRectangleShape(int deltaX, int deltaY, Image image) {
+        super(DEFAULT_X_POS, DEFAULT_Y_POS, deltaX, deltaY);
+        this.image = image;
+    }
+
+    public static Image makeImage(String imageFileName, int shapeWidth) {
+        sw = shapeWidth;
+        File f; // instance of File
+        BufferedImage inputImage, outputImage; // instance of BufferedImage
+        int w; // width of loaded image
+        int h; // height of loaded image
+        double sf; // scale factor
+        Graphics2D g; // instnace of Graphics2D
+
+        f = new File(imageFileName);
+
+        try {
+            inputImage = ImageIO.read(f);
+        } catch (IOException e) {
+            System.out.println("Image not found");
+            return null;
+        }
+
+        w = inputImage.getWidth();
+        h = inputImage.getHeight();
+
+        sf = w > sw ? (double) sw / w : 1;
+        sh = (int) (sf * h);
+
+        outputImage = new BufferedImage(sw, sh, inputImage.getType());
+        g = outputImage.createGraphics();
+        g.drawImage(inputImage, 0, 0, sw, sh, null);
+
+        return outputImage;
+    }
+
+    public void doPaint(Painter painter){
+        painter.drawImage(this.image, x(), y(), sw, sh);
+    }
+
 }
